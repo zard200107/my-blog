5fef372981434aa768617195e6120441af5359c6
gao_yangkai <zard200107@gmail.com>
2024-09-24 21:07:02 +0800

    dev:loader component-v2

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,14 +3,10 @@ import "./Loader.css";
 
 const Loader: FC = () => {
   return (
-    // body
-    <div className="flex justify-center items-center min-h-[100vh] overflow-hidden">
-      {/* container */}
-      <div className="relative w-full">
-        {/* box */}
-        <div className="relative flex justify-center items-center w-full animate-box">
-          {/* cube */}
-          <div className="relative w-[200px] h-[200px] bg-[#03e9f4] boxShadow-custom animation-cube" />
+    <div className="loader-body">
+      <div className="loader-container">
+        <div className="loader-box">
+          <div className="loader-cube" />
         </div>
       </div>
     </div>
